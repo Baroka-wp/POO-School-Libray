15612200340f2ba8ba8e8e95ae734f990ce1c6d2
baroka <birotori@gmail.com>
2022-07-21 15:24:06 +0100

    Delete unsed code in main.rb

--- main.rb
+++ main.rb
@@ -5,16 +5,3 @@ require './book'
 require './capitalize_decorator'
 require './trimmer_decorator'
 require './rental'
-
-# person = Person.new(22, 'maxime')
-# p person.correct_name
-# capitalizedPerson = CapitalizeDecorator.new(person)
-# p capitalizedPerson.correct_name
-# capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-# p capitalizedTrimmedPerson.correct_name
-
-b = Book.new('way', 'Yun')
-student = Student.new(nil, 12, 'Rio')
-student.add_rental('2021', b)
-student.add_rental('2022', b)
-p student.rentals
