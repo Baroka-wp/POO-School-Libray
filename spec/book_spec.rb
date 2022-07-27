f6be915aac9a5194f0a79977b4b24bdcba8b8642
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-07-27 09:09:34 +0100

    Created test for Books

---
+++ spec/book_spec.rb
@@ -0,0 +1,14 @@
+require './models/book'
+
+
+describe Book do
+    before :each do 
+        @book = Book.new  nil, 'way',  'yang'
+    end
+    it 'should print the book title' do
+        expect(@book.title).to eq 'way'
+    end
+    it 'should print the book author' do
+        expect(@book.author).to eq 'yang'
+    end
+end
